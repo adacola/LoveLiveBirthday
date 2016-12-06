6c08936493a3ec695637c68a1e1bcda4344f6cfb
adacola <adacola255@gmail.com>
2016-12-07 00:20:26 +0900

    中間データを生のstringではかく型をつけた

--- LoveLiveBirthday/BirthCount.fs
+++ LoveLiveBirthday/BirthCount.fs
@@ -1,23 +1,46 @@
 module Adacola.LoveLiveBirthday.BirthCount
 
 open System
+open System.Text
 open System.Text.RegularExpressions
+open System.Net
 open Basis.Core
 
+type YearContentUri = YearContentUri of Uri
+type DataContentUri = DataContentUri of Uri
+type BirthCountContentUri = BirthCountContentUri of Uri
+type YearContent = YearContent of string
+type DataContent = DataContent of string
+type BirthCountContent = BirthCountContent of string
+type BirthCounts = BirthCounts of int[]
+
+let private tryDownload encoding toDestination (uri : Uri) =
+    async {
+        try
+            use client = new WebClient(Encoding = encoding)
+            let! content = client.AsyncDownloadString(uri)
+            return content |> toDestination |> Success
+        with e -> return Failure e
+    }
+
+let tryDownloadYearContent (YearContentUri uri) = tryDownload Encoding.UTF8 YearContent uri
+let tryDownloadDataContent (DataContentUri uri) = tryDownload Encoding.UTF8 DataContent uri
+let tryDownloadBirthCountCoutent (BirthCountContentUri uri) = tryDownload (Encoding.GetEncoding "Shift_JIS") BirthCountContent uri
+
 /// e-Statの「人口動態統計 確定数 保管統計表（報告書非掲載表） 出生」の年次一覧ページから目的の年次のページのURIを取得
-let tryGetYearUri (baseUri : Uri) content (year : int<年>) =
+let tryGetYearUri (YearContentUri baseUri) (YearContent content) (year : int<年>) =
     match Regex.Match(content, sprintf """<a\s+href="(?<uri>[^"]+)"[^>]*>\s*%d年\s*</a>""" year, RegexOptions.Singleline) with
-    | m when m.Success -> Uri(baseUri, m.Groups.["uri"].Value) |> Some
+    | m when m.Success -> Uri(baseUri, m.Groups.["uri"].Value) |> DataContentUri |> Some
     | _ -> None
 
 /// e-Statの「人口動態統計 確定数 保管統計表（報告書非掲載表） 出生」の年次ページから出生年月日時のCSVのURIを取得
-let tryGetDataUri (baseUri : Uri) content =
+let tryGetDataUri (DataContentUri baseUri) (DataContent content) =
     match Regex.Match(content, """出生数，出生年月日時・出生の場所別.*?<a\s+href="(?<uri>[^"]+)"[^>]*>""", RegexOptions.Singleline) with
-    | m when m.Success -> Uri(baseUri, m.Groups.["uri"].Value) |> Some
+    | m when m.Success -> Uri(baseUri, m.Groups.["uri"].Value) |> BirthCountContentUri |> Some
     | _ -> None
 
 /// 出生年月日時のCSVから出生年月日ごとの誕生数の配列
-let tryGetBirthCounts (content : string) =
+let tryGetBirthCounts (BirthCountContent content) =
     let rows = content.Split([|"\r\n"; "\n"; "\r"|], StringSplitOptions.None) |> Array.map (Str.splitBy ",")
     let tryGetDayBirthCount index =
         if Regex.IsMatch(rows.[index].[0], @"\A.+日\z") then
@@ -39,5 +62,5 @@ let tryGetBirthCounts (content : string) =
                     | 2 -> Array.append [|xs.[0] + birthCountsTable.[1].[28]|] xs.[1 ..]
                     | _ -> xs))
             else birthCountsTable
-        return result |> Seq.concat |> Seq.toList
+        return result |> Array.concat |> BirthCounts
     }
--- LoveLiveBirthday/Birthday.fs
+++ LoveLiveBirthday/Birthday.fs
@@ -6,8 +6,8 @@ let private random = Random.mersenneTwister()
 
 /// 2014年の日別の出生数に基いて誕生日のインデックスをランダムに出力する。
 /// 1月1日を0、1月2日を1、……、12月31日を364としたインデックスを返す。
-let getRandomBirthday birthCounts =
-    let sumOfBirthCount = birthCounts |> List.sum
+let getRandomBirthday (BirthCount.BirthCounts birthCounts) =
+    let sumOfBirthCount = birthCounts |> Array.sum
     let integralBirthCounts =
         (-1, birthCounts) ||> Seq.scan (fun sum birthCount -> sum + birthCount) |> Seq.tail |> Seq.toArray
     let searchIndex n =
